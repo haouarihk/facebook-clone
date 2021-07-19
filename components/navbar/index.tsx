7b4c575e5d9b5ff9ff6f1d447da5c971c1209de5
Haouari haitam Kouider <haithem2001@gmail.com>
2021-07-19 10:05:28 +0100

    creating it up

---
+++ components/navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from'./index.module.sass';
+
+  
+export default function Navbar ()  {
+  return (
+    <nav className={styles.navbar}>
+        
+    </nav>
+  );
+};
+
+// add style 
